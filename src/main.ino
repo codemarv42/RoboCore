d457571e0ded7fd4158825aa55863b042994eac8
MasterOktagon <57826808+MasterOktagon@users.noreply.github.com>
2023-06-01 17:27:41 +0200

    Create main.ino

---
+++ src/main.ino
@@ -0,0 +1,15 @@
+/*
+copyright (c) ImmernochKeinName, 2023
+
+licensed under CC-BY 3.0
+
+view LICENSE.md for details
+*/
+void setup(){
+  //Hardware-Init
+}
+
+void loop(){
+  //everything else, keep this short
+}
+
