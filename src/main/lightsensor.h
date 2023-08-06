9096b380f29505cb2f99ae9f74f85da4b4e6c98a
MasterOktagon <userkontakt.hexagongames@gmail.com>
2023-08-06 11:05:57 +0200

    Add: Basic Line Following

--- src/main/lightsensor.h
+++ src/main/lightsensor.h
@@ -1,3 +1,5 @@
+#include "esp32-hal.h"
+#include "sys_arch.h"
 #ifndef LIGHTSENSOR_H
 #define LIGHTSENSOR_H
 
@@ -13,15 +15,20 @@ struct SensorData{
   uint16_t value=0;
 };
 
+/*inline uint16_t map(uint16_t value, uint16_t fromlow, uint16_t tolow, uint16_t tohigh){
+  return (value-fromlow)/(tolow-fromlow)*tohigh;
+}*/
 
 class LightSensor{
   private:
     uint8_t led_pin;
     void led_on(){
       shift_register::write(this->led_pin, HIGH); // TODO test and make private
+      delayMicroseconds(40);
     }
     void led_off(){
       shift_register::write(this->led_pin, LOW);
+      //delayMicroseconds(40);
     }
 
   public:
@@ -35,6 +42,7 @@ class LightSensor{
     }
     void calibrate_turn(){
       led_on();
+      delayMicroseconds(20);
       uint16_t current_value = ADCRead(ADC_PT_L_1); // TODO: change part of the ifs to else ifs
       this->left_outer.max = max(this->left_outer.max, current_value);
       this->left_outer.min = min(this->left_outer.min, current_value);
@@ -59,10 +67,11 @@ class LightSensor{
     }
     void read(){
       led_on();
+      //Serial.println(ADCRead(ADC_PT_R_0));
       this->left_outer.value  = map(ADCRead(ADC_PT_L_1), left_outer.min,  left_outer.max, 0, 100);
       this->left.value        = map(ADCRead(ADC_PT_L_0), left.min,        left.max, 0, 100);
       this->center.value      = map(ADCRead(ADC_PT_M  ), center.min,      center.max, 0, 100);
-      this->right.value       = map(ADCRead(ADC_PT_R_0), right.min,       0xFFFF, 0, 100);
+      this->right.value       = map(ADCRead(ADC_PT_R_0), right.min,       right.max, 0, 100);
       this->right_outer.value = map(ADCRead(ADC_PT_R_1), right_outer.min, right_outer.max, 0, 100);
       led_off();
     }
--- src/main/main.ino
+++ src/main/main.ino
@@ -2,6 +2,7 @@
 #include <Arduino.h>
 #include "hardware.h"
 #include "Wire.h"
+#include <time.h>
 
 /*
 copyright (c) ImmernochKeinName, 2023
@@ -14,6 +15,8 @@ view LICENSE.md for details
 
 // SPEED
 #define V 100
+#define DEBUG
+#define NOMOTORS
 
 bool HardwareInit(){
   /// get the shift register's Pins ///
@@ -35,21 +38,54 @@ void setup(){
   Serial.println("HardwareInit...");
   HardwareInit();
   Serial.println("Calibration...");
-  //calibrate(all_sensors, CALIBRATION, 3);
-  calibrate(all_sensors, 10, 300);
+  calibrate(all_sensors, 1000, 3);
   Serial.print("White Left max: "); Serial.print(white.left.max); Serial.print(" - White Right max: "); Serial.println(white.right.max);
-  delay(1000);
+  Serial.print("White Left min: "); Serial.print(white.left.min); Serial.print(" - White Right min: "); Serial.println(white.right.min);
+  delayMicroseconds(1000000);
+}
+
+int16_t diff_cache[10] = {0};
+uint32_t timestamp = millis() + 100;
+uint8_t cache_index = 0;
+
+void clear_cache(){
+  std::fill(std::begin(diff_cache), std::end(diff_cache), 0);
+}
+void cache(int16_t value){
+  if (millis() > timestamp){
+    timestamp = millis() + 100;
+    diff_cache[cache_index] = value;
+    cache_index = (cache_index + 1) % 10;
+    #ifdef DEBUG
+      Serial.println("Cached");
+    #endif
+  }
 }
 
 void loop() {
-
   for (auto sensor:all_sensors){ // read light values
     if (sensor != nullptr){sensor->read();}
   }
-  int diff = white.left.value - white.right.value;
-  motor::fwd(A, V + diff);
-  motor::fwd(B, V - diff);
-  white.read();
-  Serial.println(white.right.value);
-  
+ //Serial.print(white.right.value);
+  #define diff_outer_factor 2.5 // Factor for the outer light 
+  #define center_factor 0.1
+  #define mul 2
+  int16_t diff = white.left.value - white.right.value;
+  int16_t diff_outer = white.left_outer.value - white.right_outer.value;
+  int16_t mot_diff = (diff + diff_outer*diff_outer_factor)*mul; //* center_factor * white.center.value;
+  cache(mot_diff);
+  /*Serial.print(white.left.value);
+  Serial.print(" ");
+  Serial.print(white.right.value);
+  Serial.print(" ");
+  Serial.println(diff);*/
+  #ifndef NOMOTORS
+    motor::fwd(A, V + mot_diff);
+    motor::fwd(B, V - mot_diff);
+  #endif
+  // white.read();
+  //Serial.println(diff);
+  #ifndef NOMOTORS
+    delayMicroseconds(1000);
+  #endif 
 }
