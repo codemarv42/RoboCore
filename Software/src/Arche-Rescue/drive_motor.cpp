6bbea0ab9e0ea3a46bb626a0b331634b2f066498
Salkin-Niklas <nizdvfc@web.de>
2023-09-22 15:55:26 +0200

    drive_motor

---
+++ Software/src/Arche-Rescue/drive_motor.cpp
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+#include "drive_motor.h"
+#include "robot.h"
+
+Motor::Motor(byte pinPwm, byte pin1, byte pin2, byte pinStby) {
+    this->pinPwm = pinPwm;
+    this->pin1 = pin1;
+    this->pin2 = pin2;
+    this->pinStby = pinStby;
+}
+void Motor::init() {
+    pinMode(pinPwm, OUTPUT);
+    pinMode(pin1, OUTPUT);
+    pinMode(pin2, OUTPUT);
+    pinMode(pinStby, OUTPUT);
+    digitalWrite(pinStby, HIGH);
+}
+void Motor::Fwd(int v) {
+    if (v > 255) {
+        v = 255;
+    }
+    if (v < -255) {
+        v = -255;
+    }
+    if (v >= 0) {
+        digitalWrite(pin1, LOW);
+        digitalWrite(pin2, HIGH);
+    }
+    if (v < 0) {
+        v = -v;
+        digitalWrite(pin1, HIGH);
+        digitalWrite(pin2, LOW);
+    }
+    analogWrite(pinPwm, v);
+}
+void Motor::Rev(int v) {
+    Fwd(-v);
+}
+void Motor::Left(int v){
+    Fwd(-v);
+}
+void Motor::Right(int v){
+    Fwd(v);
+}
+void Motor::Off() {
+    Fwd(0);
+}
+void Motor::StandBy() {
+    digitalWrite(pinStby, LOW);
+}
+void Motor::WakeUp() {
+    digitalWrite(pinStby, HIGH);
+}
---
+++ Software/src/Arche-Rescue/drive_motor.h
@@ -0,0 +1,27 @@
+#ifndef DRIVE_MOTOR_H
+#define DRIVE_MOTOR_H
+#include <Arduino.h>
+
+class Motor {
+    private:
+        byte pinPwm;
+        byte pin1;
+        byte pin2;
+        byte pinStby;
+        int v;  // optional, muss nicht gespeichert werden
+
+    public:
+        Motor(byte pinPwm, byte pin1, byte pin2, byte pinStby);
+
+        void init();
+        void Fwd(int v);
+        void Rev(int v);
+        void Left(int v);
+        void Right(int v);
+        void Off();
+        void StandBy();
+        void WakeUp();
+};
+
+
+#endif
