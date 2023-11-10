20eadc898ea9129642a878c1c2b5c92a5ba5e117
schaarva <valentin.schaar@web.de>
2023-11-10 16:56:43 +0100

    Fixed rgb HIGH-LOW-bug.

--- Software/src/Arche-Rescue/rgb_led.cpp
+++ Software/src/Arche-Rescue/rgb_led.cpp
@@ -11,7 +11,7 @@ void RGB_Led::RGB_Led(byte pinR, byte pinG, byte pinG)
 
 void RGB_Led::red()
 {
-  ShiftRegisterWrite(pinR);
+  ShiftRegisterWrite(pinR, 0);
 }
 
 void RGB_Led::green()
@@ -29,40 +29,56 @@ void RGB_Led::RGB_Led(byte pinR, byte pinG, byte pinG)
 
 void RGB_Led::red()
 {
-  ShiftRegisterWrite(pinR);
+  ShiftRegisterWrite(pinR, 0);
+  ShiftRegisterWrite(pinG, 1);
+  ShiftRegisterWrite(pinB, 1);
 }
 
 void RGB_Led::green()
 {
-  ShiftRegisterWrite(pinG);
+  ShiftRegisterWrite(pinR, 1);
+  ShiftRegisterWrite(pinG, 0);
+  ShiftRegisterWrite(pinB, 1);
 }
 
 void RGB_Led::blue()
 {
-  ShiftRegisterWrite(pinB);
+  ShiftRegisterWrite(pinR, 1);
+  ShiftRegisterWrite(pinG, 1);
+  ShiftRegisterWrite(pinB, 0);
 }
 
 void RGB_Led::yellow()
 {
-  ShiftRegisterWrite(pinR);
-  ShiftRegisterWrite(pinG);
+  ShiftRegisterWrite(pinR, 0);
+  ShiftRegisterWrite(pinG, 0);
+  ShiftRegisterWrite(pinB, 1);
 }
 
 void RGB_Led::magenta()
 {
-  ShiftRegisterWrite(pinR);
-  ShiftRegisterWrite(pinB);
+  ShiftRegisterWrite(pinR, 0);
+  ShiftRegisterWrite(pinG, 1);
+  ShiftRegisterWrite(pinB, 0);
 }
 
 void RGB_Led::cyan()
 {
-  ShiftRegisterWrite(pinG);
-  ShiftRegisterWrite(pinB);
+  ShiftRegisterWrite(pinR, 1);
+  ShiftRegisterWrite(pinG, 0);
+  ShiftRegisterWrite(pinB, 0);
 }
 
 void RGB_Led::white()
 {
-  ShiftRegisterWrite(pinR);
-  ShiftRegisterWrite(pinG);
-  ShiftRegisterWrite(pinB);
+  ShiftRegisterWrite(pinR, 0);
+  ShiftRegisterWrite(pinG, 0);
+  ShiftRegisterWrite(pinB, 0);
+}
+
+void RGB_Led::off()
+{
+  ShiftRegisterWrite(pinR, 1);
+  ShiftRegisterWrite(pinG, 1);
+  ShiftRegisterWrite(pinB, 1);
 }
