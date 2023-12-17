5f8342f39f1570fcb6c045114c1a018243d221a0
MasterOktagon <=>
2023-12-17 18:35:34 +0100

    CHANGE: improve TOF ranging speed timeout

--- src/main/tof.cpp
+++ src/main/tof.cpp
@@ -26,14 +26,49 @@ namespace tof{
     
     shift_register::write(SR_XSHT1, HIGH);
     delay(10);
-    if (!left.init()){Serial.print("dumm");}
-    
+    if (!left.init()){Serial.print("dumm 1");}
+    shift_register::write(SR_XSHT1, LOW, true);
+    shift_register::write(SR_XSHT2, HIGH);
+    delay(10);
+    if (!claw.init()){Serial.print("dumm 2");}
+    shift_register::write(SR_XSHT3, HIGH, true);
+    shift_register::write(SR_XSHT2, LOW);
+    delay(10);
+    if (!turnable_upper.init()){Serial.print("dumm 3");}
+    shift_register::write(SR_XSHT4, HIGH, true);
+    shift_register::write(SR_XSHT3, LOW);
+    delay(10);
+    if (!turnable_lower.init()){Serial.print("dumm 4");}
+    shift_register::write(SR_XSHT4, LOW);
+
     Serial.println("All tof Sensors active!");
 
-    left.startContinuous(50);
+    /*left.startContinuous(50);
     claw.startContinuous(50);
     turnable_upper.startContinuous(50);
-    turnable_lower.startContinuous(50);
+    turnable_lower.startContinuous(50);*/
+  }
+
+  uint16_t readLeft(){
+    shift_register::write(SR_XSHT1, HIGH);
+    delay(1);
+    uint16_t a = left.readSingle();
+    shift_register::write(SR_XSHT1, LOW, true);
+    return a;
+  }
+  uint16_t readUpper(){
+    shift_register::write(SR_XSHT3, HIGH);
+    delay(1);
+    uint16_t a = turnable_upper.readSingle();
+    shift_register::write(SR_XSHT3, LOW, true);
+    return a;
+  }
+  uint16_t readLower(){
+    shift_register::write(SR_XSHT4, HIGH);
+    delay(10);
+    uint16_t a = turnable_lower.readSingle();
+    shift_register::write(SR_XSHT4, LOW, true);
+    return a;
   }
 }
 
--- src/main/tof.h
+++ src/main/tof.h
@@ -1,3 +1,4 @@
+#include <cstdint>
 #pragma once
 #include <Arduino.h>
 #include <Wire.h>
@@ -7,4 +8,7 @@
 namespace tof{
   extern void init();
   extern VL53L1X left, claw, turnable_upper, turnable_lower;
+  extern uint16_t readLeft();
+  extern uint16_t readUpper();
+  extern uint16_t readLower();
 }
