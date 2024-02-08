6fd5b73bf046cffbabfa15a1c61149179c390713
MasterOktagon <=>
2024-02-08 11:27:05 +0100

    REFACTOR: Lightsensor class now correctly split into .h and .cpp

--- src/main/lightsensor.cpp
+++ src/main/lightsensor.cpp
@@ -12,7 +12,6 @@
 
 void calibrate(LightSensor* sensors[],const int amount = CALIBRATION,const int time_between_read_ms = 3)
 {
-  //motor::fwd(AB,1);
   for(int i = 0; i < amount; i++) {
     for(int j = 0; j < 4; j++) {
       if (sensors[j] != nullptr) {
@@ -24,6 +23,55 @@ void calibrate(LightSensor* sensors[],const int amount = CALIBRATION,const int t
   }
 }
 
+void LightSensor::led_on(){
+  shift_register::write(led_pin, HIGH);
+  delayMicroseconds(80);
+}
+
+void LightSensor::led_off(){
+  delayMicroseconds(20);
+  shift_register::write(led_pin, LOW, true);
+}
+inline int16_t LightSensor::map(int16_t value, int16_t minv, int16_t maxv){
+  return ((value - minv) * 100) / (maxv - minv);
+}
+LightSensor::LightSensor(int led_pin){
+  led_pin = led_pin;
+}
+
+void LightSensor::calibrate_turn(){
+  led_on();
+  int16_t current_value = ADCRead(ADC_PT_L_1); // TODO: change part of the ifs to else ifs
+  left_outer.max = max(left_outer.max, current_value);
+  left_outer.min = min(left_outer.min, current_value);
+  
+  current_value = ADCRead(ADC_PT_L_0);
+  left.max = max(left.max, current_value);
+  left.min = min(left.min, current_value);
+  
+  current_value = ADCRead(ADC_PT_M);
+  center.max = max(center.max, current_value);
+  center.min = min(center.min, current_value);
+  
+  current_value = ADCRead(ADC_PT_R_0);
+  right.max = max(right.max, current_value);
+  right.min = min(right.min, current_value);
+  
+  current_value = ADCRead(ADC_PT_R_1);
+  right_outer.max = max(right_outer.max, current_value);
+  right_outer.min = min(right_outer.min, current_value);
+  led_off();
+}
+
+void LightSensor::read(){
+  led_on();
+  left_outer.value  = map(ADCRead(ADC_PT_L_1), left_outer.min,  left_outer.max);
+  left.value        = map(ADCRead(ADC_PT_L_0), left.min,        left.max);
+  center.value      = map(ADCRead(ADC_PT_M  ), center.min,      center.max);
+  right.value       = map(ADCRead(ADC_PT_R_0), right.min,       right.max);
+  right_outer.value = map(ADCRead(ADC_PT_R_1), right_outer.min, right_outer.max);
+  led_off();
+}
 
 DirectSensor::DirectSensor(uint8_t setLedPin, uint8_t setAdcPin){
   ledPin = setLedPin;
--- src/main/lightsensor.h
+++ src/main/lightsensor.h
@@ -13,7 +13,7 @@
 
 
 struct SensorData{
-  int16_t min=0xFFFF; // max value vor a 16-Bit integer in Hexadecimal
+  int16_t min=0x7FFF; // max value vor a 16-Bit integer in Hexadecimal
   int16_t max=0;
   int16_t value=0;
 };
@@ -21,18 +21,10 @@ struct SensorData{
 class LightSensor{
   private:
     uint8_t led_pin;
-    void led_on(){
-      shift_register::write(this->led_pin, HIGH); // TODO test and make private
-      delayMicroseconds(80);
-    }
-    void led_off(){
-      delayMicroseconds(20);
-      shift_register::write(this->led_pin, LOW, true);
-    }
 
-    inline int16_t map(int16_t value, int16_t minv, int16_t maxv){
-      return ((value - minv) * 100) / (maxv - minv);
-    }
+    void led_on();
+    void led_off();
+    inline int16_t map(int16_t value, int16_t minv, int16_t maxv);
 
   public:
     SensorData left_outer;
@@ -40,43 +32,10 @@ class LightSensor{
     SensorData center;
     SensorData right;
     SensorData right_outer;
-    LightSensor(int led_pin){
-      this->led_pin = led_pin;
-    }
-    void calibrate_turn(){
-      led_on();
-      int16_t current_value = ADCRead(ADC_PT_L_1); // TODO: change part of the ifs to else ifs
-      this->left_outer.max = max(this->left_outer.max, current_value);
-      this->left_outer.min = min(this->left_outer.min, current_value);
-      
-      current_value = ADCRead(ADC_PT_L_0);
-      this->left.max = max(this->left.max, current_value);
-      this->left.min = min(this->left.min, current_value);
-      
-      current_value = ADCRead(ADC_PT_M);
-      this->center.max = max(this->center.max, current_value);
-      this->center.min = min(this->center.min, current_value);
-      
-      current_value = ADCRead(ADC_PT_R_0);
-      //Serial.println(ADCRead(ADC_PT_R_0));
-      this->right.max = max(this->right.max, current_value);
-      this->right.min = min(this->right.min, current_value);
-      
-      current_value = ADCRead(ADC_PT_R_1);
-      this->right_outer.max = max(this->right_outer.max, current_value);
-      this->right_outer.min = min(this->right_outer.min, current_value);
-      led_off();
-    }
-    void read(){
-      led_on();
-      this->left_outer.value  = map(ADCRead(ADC_PT_L_1), left_outer.min,  left_outer.max);
-      this->left.value        = map(ADCRead(ADC_PT_L_0), left.min,        left.max);
-      this->center.value      = map(ADCRead(ADC_PT_M  ), center.min,      center.max);
-      this->right.value       = map(ADCRead(ADC_PT_R_0), right.min,       right.max);
-      this->right_outer.value = map(ADCRead(ADC_PT_R_1), right_outer.min, right_outer.max);
-      led_off();
-    }
 
+    LightSensor(int led_pin);
+    void calibrate_turn();
+    void read();
 };
 
 class DirectSensor{
